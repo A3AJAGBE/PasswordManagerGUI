779f90fc863347aeeaf3e6b8cc07224d49d2c179
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-13 11:58:56 +0000

    Add UI initial setup

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+# App Interface
+interface = Tk()
+interface.title("A3AJAGBE OFFLINE PASSWORD-MANAGER")
+interface.config(padx=100, pady=50, bg="white")
+
+
+# Keep the app open until exited
+interface.mainloop()
