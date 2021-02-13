26a9088ae2b9cf053deafd1cd6a45b74a67f966a
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-13 13:34:28 +0000

    Add password generate function

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
+import random
 from tkinter import *
 from tkinter import messagebox
+
+
+def generate_password():
+    """This function generates a random length password"""
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '@']
+
+    password = []
+
+    for num in range(2, 4):
+        num_output = random.choice(numbers)
+        password += num_output
+
+    for symbol in range(2, 4):
+        symbol_output = random.choice(symbols)
+        password += symbol_output
+
+    for letter in range(6, 10):
+        letter_output = random.choice(letters)
+        password += letter_output
+
+    # Shuffle the generated password
+    random.shuffle(password)
+
+    # Convert the list to string
+    password_generated = "".join(password)
+    # Add the password generated to the password entry
+    password_entry.insert(0, password_generated)
 
 
 def save():
@@ -65,7 +97,7 @@ password_label.grid(column=0, row=3)
 password_entry = Entry(width=30)
 password_entry.grid(column=1, row=3)
 
-generate_button = Button(text="Generate Password", padx=5, pady=5)
+generate_button = Button(text="Generate Password", padx=5, pady=5, command=generate_password)
 generate_button.grid(column=0, row=4)
 
 add_button = Button(text="Add Information", padx=5, pady=5, fg="#fc1352", command=save)
