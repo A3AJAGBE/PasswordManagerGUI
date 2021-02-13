4f2cc839563d7edea306db4c1b446e37faaa532a
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-13 12:02:49 +0000

    Add image

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ from tkinter import *
 # App Interface
 interface = Tk()
 interface.title("A3AJAGBE OFFLINE PASSWORD-MANAGER")
-interface.config(padx=100, pady=50, bg="white")
+interface.config(padx=20, pady=20, bg="white")
 
+# Add image using canvas widget
+canvas = Canvas(width=125, height=125)
+bg_image = PhotoImage(file="lock.png")
+canvas.create_image(65, 65, image=bg_image)
+canvas.grid(column=1, row=1)
 
 # Keep the app open until exited
 interface.mainloop()
